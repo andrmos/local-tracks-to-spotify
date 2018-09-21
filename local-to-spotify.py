89a6916ccff88099c46c57c19fce17ce8c9089a3
andrmos <mossige.a@gmail.com>
2018-09-21 13:56:35 +0200

    Add local tracks to playlist

--- local-to-spotify.py
+++ local-to-spotify.py
@@ -3,6 +3,7 @@ import os
 import configparser
 import spotipy
 import spotipy.util
+from spotipy.client import SpotifyException
 from spotipy.oauth2 import SpotifyClientCredentials
 from tinytag import TinyTag
 
@@ -10,6 +11,7 @@ from tinytag import TinyTag
 class LocalToSpotify:
     def __init__(self, config_file_name):
         self.read_config(config_file_name)
+        self.spotify = self.authorize()
 
     def read_config(self, config_file_name):
         config = configparser.ConfigParser()
@@ -24,44 +26,44 @@ class LocalToSpotify:
             print(f'Error reading {config_file_name}.\nRefer to config.ini.example for correct configuration.')
             sys.exit()
 
-        print(f'Successfully read {config_file_name}.')
 
-
-    def print_tracks(self, path):
+    def get_tracks_in_folder(self, path):
         with os.scandir(path) as it:
+            tracks = []
             for entry in it:
                 if entry.is_file():
                     tag = TinyTag.get(entry.path)
-                    search_spotify(tag.artist, tag.title)
+                    track = { 'artist': tag.artist, 'track_title': tag.title }
+                    tracks.append(track)
+            return tracks
 
-    def search_spotify(self, artist, track):
+
+    def find_track(self, artist, track):
         search_string = f'artist:{artist} track:{track}'
-        print(f'Searching for "{search_string}"')
-
-        results = spotify.search(q=search_string)
+        results = self.spotify.search(q=search_string)
         tracks = results['tracks']['items']
 
         if len(tracks) == 0:
-            print('Found none')
-        else:
-            print(f'Found {len(tracks)} tracks')
+            print(f'{artist} - {track} was not found')
+            return None
 
-        for index, track in enumerate(tracks):
-            print(f'{index + 1}:')
-            id = track['id']
-            name = track['name']
-            artists = ', '.join([artist['name'] for artist in track['artists']])
-            print(f'Name: {name}\nArtists: {artists}\nID: {id}')
+        # TODO: Fix, currently we just add first one.
+        #       Figure out which one to add.
+        id = tracks[0]['id']
+        track_title = tracks[0]['name']
+        artists = ', '.join([artist['name'] for artist in tracks[0]['artists']])
+        track = { 'id': id, 'track_title': track_title, 'artists': artists }
+        return track
 
-        print('\n')
 
     def create_playlist(self):
         playlist_name = 'Test playlist'
         is_public = False
         description = 'This is a test playlist'
-        spotify.user_playlist_create(self.user_id, playlist_name, public = is_public)
+        self.spotify.user_playlist_create(self.user_id, playlist_name, public = is_public)
 
-    def authorize(self, scope):
+    def authorize(self):
+        scope = 'playlist-modify-public playlist-modify-private playlist-read-private'
         redirect_port = 43019
         redirect_uri = f'http://127.0.0.1:{redirect_port}/redirect'
         token = spotipy.util.prompt_for_user_token(self.user_id, scope, client_id = self.client_id, client_secret = self.client_secret, redirect_uri = redirect_uri)
@@ -73,9 +75,33 @@ class LocalToSpotify:
             print(f'Can\'t get token for {self.user_id}')
             sys.exit()
 
+    # TODO: Playlist id in config or create new playlist.
+    def add_tracks_to_playlist(self, playlist_id, tracks):
+        '''
+        tracks: dict with fields: id, track_title, artists
+        '''
+        # TODO: If not exists in playlist already
+        track_ids = [track['id'] for track in tracks]
+        try:
+            if len(track_ids) == 0:
+                return
+            self.spotify.user_playlist_add_tracks(self.user_id, playlist_id, track_ids)
+
+            track_titles = ", ".join([track['track_title'] for track in tracks])
+            print(f'Successfully added {track_titles}')
+
+        except SpotifyException as error:
+            print(error)
+            # TODO: If playlist not found, create it?
+
 
 if __name__ == '__main__':
     path = './tracks'
-    scope = 'playlist-modify-public playlist-modify-private'
     localToSpotify = LocalToSpotify('config.ini')
-    spotipy = localToSpotify.authorize(scope)
+
+    local_tracks = localToSpotify.get_tracks_in_folder(path)
+    for track in local_tracks:
+        spotify_track = localToSpotify.find_track(track['artist'], track['track_title'])
+        if spotify_track is not None:
+            playlist_id = '6u8zVlsX9YTnaOfmdAaTNR'
+            localToSpotify.add_tracks_to_playlist(playlist_id, [spotify_track])
