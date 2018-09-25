63efb58615818e6f8a9f228ec782636c976adb64
andrmos <mossige.a@gmail.com>
2018-09-25 13:04:37 +0200

    Move Track class to own file

--- LocalToSpotify.py
+++ LocalToSpotify.py
@@ -5,32 +5,7 @@ import spotipy.util
 from spotipy.client import SpotifyException
 from spotipy.oauth2 import SpotifyClientCredentials
 from MixxxExportReader import *
-
-class Track:
-    def __init__(self, id, title, artists):
-        self.id = id
-        self.title = title
-        self.artists = artists
-
-    def clean_track(self):
-        self.remove_general_artist_words()
-        self.remove_general_title_words()
-        return self
-
-    def remove_general_title_words(self):
-        words_to_remove = ['original', 'mix', 'feat', 'ft.', 'feat.', 'featuring', '&']
-        words = self.title.split(' ')
-        self.title = ' '.join([word for word in words if word not in words_to_remove]).strip()
-        return self
-
-    def remove_general_artist_words(self):
-        words_to_remove = ['original', 'mix', 'feat', 'ft.', 'feat.', 'featuring', '&']
-        words = self.artists.split(' ')
-        self.artists = ' '.join([word for word in words if word not in words_to_remove]).strip()
-        return self
-
-    def __str__(self):
-        return f'{self.id}: {self.artists} - {self.title}'
+from Track import *
 
 class LocalToSpotify:
     def __init__(self, config_file_name):
---
+++ Track.py
@@ -0,0 +1,26 @@
+class Track:
+    def __init__(self, id, title, artists):
+        self.id = id
+        self.title = title
+        self.artists = artists
+
+    def clean_track(self):
+        self.remove_general_artist_words()
+        self.remove_general_title_words()
+        return self
+
+    def remove_general_title_words(self):
+        words_to_remove = ['original', 'mix', 'feat', 'ft.', 'feat.', 'featuring', '&']
+        words = self.title.split(' ')
+        self.title = ' '.join([word for word in words if word not in words_to_remove]).strip()
+        return self
+
+    def remove_general_artist_words(self):
+        words_to_remove = ['original', 'mix', 'feat', 'ft.', 'feat.', 'featuring', '&']
+        words = self.artists.split(' ')
+        self.artists = ' '.join([word for word in words if word not in words_to_remove]).strip()
+        return self
+
+    def __str__(self):
+        return f'{self.id}: {self.artists} - {self.title}'
+
